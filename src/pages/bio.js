bb58bcb95133faf53642a4c7f163c69d8e239647
Kassian Wren <the@nodebotani.st>
2022-10-28 10:00:58 -0500

    Added some pages

---
+++ src/pages/bio.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const BioPage = () => {
+  return (
+    <main>
+      <h1>My Bio</h1>
+      <p>I live somewhere and my hobbies are something and something else.</p>
+    </main>
+  )
+}
+
+export default BioPage
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
