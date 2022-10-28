6e9520be5c1368f2f67da32a18d2c9f6e11b01a4
Kassian Wren <the@nodebotani.st>
2022-10-28 10:08:23 -0500

    Added Links

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const BioPage = () => {
   return (
     <main>
       <h1>My Bio</h1>
       <p>I live somewhere and my hobbies are something and something else.</p>
+      <Link to="/">Back Home</Link>
     </main>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Link to="/bio">My Bio</Link>
     </main>
   )
 }
